0c2c8b8c2edfad150e904d677053861765e03a20
Will Abernathy <matthew.abernathy@hotmail.com>
2021-03-30 14:52:32 -0500

    updated wheat field and blood grove bounds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 /* eslint-disable no-lone-blocks */
 import { useState, useEffect } from "react";
-import './App.css';
-import { tileType, minTotalCards, maxTotalCards, placeholderDeck, roadCards, roadsideCards, landscapeCards, specialCards, goldCards, classChoice, totalCards } from './constants/constants';
-//import DeckSleeve from './Components/DeckSleeve/DeckSleeve';
+import "./App.css";
+import {
+  tileType,
+  minTotalCards,
+  maxTotalCards,
+  placeholderDeck,
+  roadCards,
+  roadsideCards,
+  landscapeCards,
+  specialCards,
+  goldCards,
+  classChoice,
+  totalCards,
+} from "./constants/constants";
 
 // randomize the deck
 const randomizeCards = () => {
   //e.preventDefault();
 
   // loop to ensure constraints of 15 max and 7 min cards are met
-  let roadTotal, roadsideTotal, landscapeTotal, specialTotal, goldTotal, classTotal, cardTotalSum;
+  let roadTotal,
+    roadsideTotal,
+    landscapeTotal,
+    specialTotal,
+    goldTotal,
+    classTotal,
+    cardTotalSum;
   do {
     // get the amount of cards for each "sleeve"
     // this is per the actual documentation.....
-    roadTotal = Math.floor(Math.random() * (Math.floor(6) - Math.ceil(2) + 1) + Math.ceil(2));
-    roadsideTotal = Math.floor(Math.random() * (Math.floor(9) - Math.ceil(2) + 1) + Math.ceil(2));
-    landscapeTotal = Math.floor(Math.random() * (Math.floor(6) - Math.ceil(2) + 1) + Math.ceil(2));
-    specialTotal = Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1));
+    roadTotal = Math.floor(
+      Math.random() * (Math.floor(6) - Math.ceil(2) + 1) + Math.ceil(2)
+    );
+    roadsideTotal = Math.floor(
+      Math.random() * (Math.floor(9) - Math.ceil(2) + 1) + Math.ceil(2)
+    );
+    landscapeTotal = Math.floor(
+      Math.random() * (Math.floor(6) - Math.ceil(2) + 1) + Math.ceil(2)
+    );
+    specialTotal = Math.floor(
+      Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1)
+    );
 
     cardTotalSum = roadTotal + roadsideTotal + landscapeTotal + specialTotal;
-  } while (cardTotalSum > maxTotalCards || cardTotalSum < minTotalCards)
+  } while (cardTotalSum > maxTotalCards || cardTotalSum < minTotalCards);
 
   goldTotal = 1;
   classTotal = 1;
@@ -33,8 +58,22 @@ const randomizeCards = () => {
   let classChoiceTemp = classChoice.slice();
 
   // set up array of arrays containing generated totals and all card values
-  let allCards = [roadCardsTemp, roadsideCardsTemp, landscapeCardsTemp, specialCardsTemp, goldCardsTemp, classChoiceTemp];
-  let allCardTotals = [roadTotal, roadsideTotal, landscapeTotal, specialTotal, goldTotal, classTotal];
+  let allCards = [
+    roadCardsTemp,
+    roadsideCardsTemp,
+    landscapeCardsTemp,
+    specialCardsTemp,
+    goldCardsTemp,
+    classChoiceTemp,
+  ];
+  let allCardTotals = [
+    roadTotal,
+    roadsideTotal,
+    landscapeTotal,
+    specialTotal,
+    goldTotal,
+    classTotal,
+  ];
 
   // prepare array of arrays to populate and return
   let roadResults = [];
@@ -45,31 +84,43 @@ const randomizeCards = () => {
   let classResults = [];
 
   // empty 2d array to return for state change
-  let resultingCards = [roadResults, roadsideResults, landscapeResults, specialResults, goldResults, classResults,
-    cardTotalSum];
+  let resultingCards = [
+    roadResults,
+    roadsideResults,
+    landscapeResults,
+    specialResults,
+    goldResults,
+    classResults,
+    cardTotalSum,
+  ];
 
   // populate randomized deck given generated data
   var i;
   for (i = 0; i < allCards.length; i++) {
     var j;
-    let randomCard;
+    var randomCard;
     for (j = 0; j < allCardTotals[i]; j++) {
       // generate random number from 0 to size of array
-      randomCard = Math.floor(Math.random() * (Math.floor(allCards[i].length) - Math.ceil(0)) + Math.ceil(0));
+      randomCard = Math.floor(
+        Math.random() * (Math.floor(allCards[i].length) - Math.ceil(0)) +
+          Math.ceil(0)
+      );
 
       // check if wheat field was selected and then add village if it hasn't been added yet
       if (allCards[i][randomCard] === "Wheat Fields") {
-        if (allCards[0].indexOf("Village") > -1) {
+        if (allCards[0].indexOf("Village") > -1 && cardTotalSum < 14) {
           resultingCards[0].push("Village");
           resultingCards[0].push(allCards[i][randomCard]);
           allCards[0].splice(randomCard, 1);
           allCards[0].splice(allCards[i].indexOf("Village"), 1);
           cardTotalSum++;
-        }
-        else {
-          // skip wheat field, too many cards for wheat fields AND village
+        } else if (resultingCards[i].indexOf("Village") > -1) {
+          resultingCards[i].push(allCards[i][randomCard]);
+          allCards[i].splice(randomCard, 1);
+        } else {
+          // skip wheat field
           j--;
-          console.log('do nothing wheat fields');
+          allCards[0].splice(allCards[i].indexOf("Wheat Fields"), 1);
         }
       }
       // check if blood grove was selected and if conditions to select it are met
@@ -78,52 +129,52 @@ const randomizeCards = () => {
         if (resultingCards[0].indexOf("Grove") > -1 && cardTotalSum < 15) {
           resultingCards[1].push(allCards[1][randomCard]);
           allCards[1].splice(randomCard, 1);
-        }
-        else {
+        } else {
           // skip blood grove
           j--;
-          console.log('do nothing grove');
+          allCards[1].splice(allCards[1].indexOf("Blood Grove"), 1);
         }
-      }
-      else {
+      } else {
         // add randomly selected card to array of arrays containing all selected cards
         resultingCards[i].push(allCards[i][randomCard]);
 
         // remove randomly selected card(s) to avoid duplicates
         allCards[i].splice(randomCard, 1);
       }
-
     }
-
   }
 
-  resultingCards[6] = resultingCards[0].length + resultingCards[1].length + resultingCards[2].length + resultingCards[3].length;
+  resultingCards[6] =
+    resultingCards[0].length +
+    resultingCards[1].length +
+    resultingCards[2].length +
+    resultingCards[3].length;
 
   // set the randomized deck as state
-  //console.log(resultingCards);
+  console.log(resultingCards);
   return resultingCards;
-}
+};
 
 // randomize the chapter from 1-4
 const randomizeChapter = () => {
-  let randomChapter = Math.floor(Math.random() * (Math.floor(4) - Math.ceil(1) + 1) + Math.ceil(1));
+  let randomChapter = Math.floor(
+    Math.random() * (Math.floor(4) - Math.ceil(1) + 1) + Math.ceil(1)
+  );
 
   return randomChapter;
-}
+};
 
-
-
-
-{/* 
+{
+  /* 
   Main Component
-*/}
+*/
+}
 const App = () => {
-
   // states used through app
   // our randomized deck, the randomized chapter and the page font
   const [randomizedDeck, setRandomizedDeck] = useState(placeholderDeck);
   const [randomizedChapter, setRandomizedChapter] = useState(0);
-  const [pageFont, setPageFont] = useState('ARCADECLASSIC');
+  const [pageFont, setPageFont] = useState("ARCADECLASSIC");
   // eslint-disable-next-line no-unused-vars
   const [pictures, setPictures] = useState(totalCards);
 
@@ -144,14 +195,19 @@ const App = () => {
   // update the page font using event handler
   const changeFont = (event) => {
     setPageFont(event.target.value);
-  }
+  };
 
   return (
     <div className="App" style={{ fontFamily: pageFont }}>
       <h1 className="title">Loop Hero Randomizer</h1>
 
       <div>
-        <select className="fontSelect" value={pageFont} onChange={changeFont} style={{ fontFamily: pageFont }}>
+        <select
+          className="fontSelect"
+          value={pageFont}
+          onChange={changeFont}
+          style={{ fontFamily: pageFont }}
+        >
           <option value="ARCADECLASSIC">Retro</option>
           <option value="roboto">Simple</option>
           <option value="dyslex">Dyslexic</option>
@@ -160,34 +216,89 @@ const App = () => {
 
       <table className="table">
         <tbody>
-
           <tr>
             <th>
               <h2 className="cardsColumnsTitle">Cards</h2>
-              <button className="randomizeButton" style={{ fontFamily: pageFont }} onClick={() => setRandomizedDeck(randomizeCards)}>Randomize</button>
+              <button
+                className="randomizeButton"
+                style={{ fontFamily: pageFont }}
+                onClick={() => setRandomizedDeck(randomizeCards)}
+              >
+                Randomize
+              </button>
             </th>
           </tr>
 
           <tr>
             <td className="cardsColumns">
               <h2>Total Cards: [{randomizedDeck[6]}]</h2>
-              <h2>{tileType[0]} [{randomizedDeck[0].length}]</h2>
-              {randomizedDeck[0].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              <h2>
+                {tileType[0]} [{randomizedDeck[0].length}]
+              </h2>
+              {randomizedDeck[0].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
 
-              <h2>{tileType[1]} [{randomizedDeck[1].length}]</h2>
-              {randomizedDeck[1].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              <h2>
+                {tileType[1]} [{randomizedDeck[1].length}]
+              </h2>
+              {randomizedDeck[1].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
 
-              <h2>{tileType[2]} [{randomizedDeck[2].length}]</h2>
-              {randomizedDeck[2].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              <h2>
+                {tileType[2]} [{randomizedDeck[2].length}]
+              </h2>
+              {randomizedDeck[2].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
 
-              <h2>{tileType[3]} [{randomizedDeck[3].length}]</h2>
-              {randomizedDeck[3].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              <h2>
+                {tileType[3]} [{randomizedDeck[3].length}]
+              </h2>
+              {randomizedDeck[3].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
 
               <h2>{tileType[4]}</h2>
-              {randomizedDeck[4].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              {randomizedDeck[4].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
 
               <h2>{tileType[5]}</h2>
-              {randomizedDeck[5].map((card, index) => (<img className="cardImage" key={index} src={require(`./assets/${card}.webp`).default} alt={card} />))}
+              {randomizedDeck[5].map((card, index) => (
+                <img
+                  className="cardImage"
+                  key={index}
+                  src={require(`./assets/${card}.webp`).default}
+                  alt={card}
+                />
+              ))}
             </td>
           </tr>
           <tr>
@@ -197,15 +308,26 @@ const App = () => {
           </tr>
           <tr>
             <td className="otherColumn">
-              <button className="randomizeButton" style={{ fontFamily: pageFont }} onClick={() => setRandomizedChapter(randomizeChapter)}>Randomize</button>
+              <button
+                className="randomizeButton"
+                style={{ fontFamily: pageFont }}
+                onClick={() => setRandomizedChapter(randomizeChapter)}
+              >
+                Randomize
+              </button>
               <h2>Chapter : [{randomizedChapter}]</h2>
             </td>
           </tr>
-
         </tbody>
       </table>
 
-      <footer className="simpleFooter">Code at: <a href="https://github.com/williammabernathy/Loop-Hero-Randomizer"> https://github.com/williammabernathy/Loop-Hero-Randomizer </a></footer>
+      <footer className="simpleFooter">
+        Code at:{" "}
+        <a href="https://github.com/williammabernathy/Loop-Hero-Randomizer">
+          {" "}
+          https://github.com/williammabernathy/Loop-Hero-Randomizer{" "}
+        </a>
+      </footer>
 
       {/*
       The start of making it "pretty"
@@ -220,9 +342,8 @@ const App = () => {
         }
       </ul>
       */}
-
     </div>
   );
-}
+};
 
 export default App;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,82 @@
-export const tileType = ["Road", "Roadside", "Landscape", "Special", "Golden", "Class"];
+export const tileType = [
+  "Road",
+  "Roadside",
+  "Landscape",
+  "Special",
+  "Golden",
+  "Class",
+];
 
 export const minTotalCards = 7;
 export const maxTotalCards = 15;
 
-export const placeholderDeck = [[], [], [], [], [], [], ['00'], ['00'], ['00'], ['00'], ['00']];
+export const placeholderDeck = [
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  ["00"],
+  ["00"],
+  ["00"],
+  ["00"],
+  ["00"],
+];
 
-export const roadCards = ["Cemetery", "Village", "Grove", "Wheat Fields", "Swamp", "Ruins"];
+export const roadCards = [
+  "Cemetery",
+  "Village",
+  "Grove",
+  "Wheat Fields",
+  "Swamp",
+  "Ruins",
+];
 roadCards.name = "Road";
-export const roadsideCards = ["Spider Cocoon", "Vampire Mansion", "Battlefield", "Blood Grove", "Bookery", "Road Lantern", "Smith Forge", "Chrono Crystals", "Outpost"];
+export const roadsideCards = [
+  "Spider Cocoon",
+  "Vampire Mansion",
+  "Battlefield",
+  "Blood Grove",
+  "Bookery",
+  "Road Lantern",
+  "Smith Forge",
+  "Chrono Crystals",
+  "Outpost",
+];
 roadsideCards.name = "Roadside";
-export const landscapeCards = ["Mountain", "Thicket", "River", "Sand Dunes", "Meadow", "Suburbs"];
+export const landscapeCards = [
+  "Mountain",
+  "Thicket",
+  "River",
+  "Sand Dunes",
+  "Meadow",
+  "Suburbs",
+];
 landscapeCards.name = "Landscape";
-export const specialCards = ["Oblivion", "Beacon", "Storm Temple", "Temporal Beacon", "Treasury"];
+export const specialCards = [
+  "Oblivion",
+  "Beacon",
+  "Storm Temple",
+  "Temporal Beacon",
+  "Treasury",
+];
 specialCards.name = "Special";
-export const goldCards = ["Ancestral Crypt", "Zero Milestone", "Maze Of Memories", "Arsenal"];
+export const goldCards = [
+  "Ancestral Crypt",
+  "Zero Milestone",
+  "Maze Of Memories",
+  "Arsenal",
+];
 goldCards.name = "Gold";
 export const classChoice = ["Warrior", "Rogue", "Necromancer"];
-classChoice.name = "Class"
+classChoice.name = "Class";
 
-export const totalCards = [roadCards.slice(), roadsideCards.slice(), landscapeCards.slice(), specialCards.slice(), goldCards.slice(), classChoice.slice()];
+export const totalCards = [
+  roadCards.slice(),
+  roadsideCards.slice(),
+  landscapeCards.slice(),
+  specialCards.slice(),
+  goldCards.slice(),
+  classChoice.slice(),
+];
